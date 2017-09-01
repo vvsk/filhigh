7769a9727b86f87623c9e72adde5dad120edb2d3
V.Venkat Sai Kiran <vvsk.1988@gmail.com>
2017-09-01 08:07:53 +0530

    Add files via upload

---
+++ min.js
@@ -0,0 +1,18 @@
+// function loadMap() {
+//     var options = {
+//         zoom: 12,
+//         mapTypeId: google.maps.MapTypeId.ROADMAP,
+//         mapTypeControl: true,
+//         scrollwheel: 1
+//     };
+//     map = new google.maps.Map(document.getElementById('map'), options);
+//     var mapCenter = new google.maps.LatLng(6.473500, 81.236577);
+//     map.setCenter(mapCenter, 12);
+//     var infowindow = new google.maps.InfoWindow({
+//         content: "You start here",
+//         position: new google.maps.LatLng(6.473500, 81.236577)
+//     });
+//     infowindow.open(map);
+
+// }
+// window.onload = loadMap();
